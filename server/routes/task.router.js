00cb9b722afeee745f95c35f7d629db205a15094
Ken Dang <k3ndang@gmail.com>
2022-01-08 21:36:08 -0600

    added both GET and POST in router side

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -3,5 +3,32 @@ const tasksRouter = express.Router();
 const pool = require('../modules/pool');
 
 
+// POST endpoint
+tasksRouter.post('/', (req, res) => {
+    let newTask = req.body;
+    console.log('in POST add new koala', newTask);
+
+    let queryText = `
+        INSERT INTO "tasks"
+            ("task-to-do", "description")
+        VALUES ($1, $2);
+    `;
+
+    let queryParams = [
+        newTask.task,
+        newTask.description,
+    ];
+
+    pool.query(queryText, queryParams)
+        .then(() => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('ERROR POST new task 👎', err);
+            
+            res.sendStatus(500);
+        });
+}); // end POST endpoint
+
 // export everything to server side
 module.exports = tasksRouter;
